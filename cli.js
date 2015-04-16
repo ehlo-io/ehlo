1c9af6367e79a375807b2a9027019f7b804c4e26
Sébastien ELET <sebastien@elet.fr>
2015-04-16 17:22:14 +0200

    Setup the smtp server

--- cli.js
+++ cli.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
 'use strict';
+
+var ehlo = require('./ehlo.js');
+console.log(ehlo);
+ehlo.start();
+
+process.on('SIGINT', function() {
+  ehlo.stop();
+  process.exit();
+});
