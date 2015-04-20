7c86be75a45f99c1c8c4ed6340a40d33eca85c82
Sébastien ELET <sebastien@elet.fr>
2015-04-20 18:02:17 +0200

    Add test for parsemiddleware

--- test/middlewares.test.js
+++ test/middlewares.test.js
@@ -13,27 +13,48 @@ describe('middleware', function() {
     ;
 
     var middleware = require('../lib/middleware.api');
-    middleware({api: 'testapi', json: {from: 'from@ehlo.io'}}, {}, function() {
-      assert.strictEqual(request.post.callCount, 1);
-      var args = request.post.args[0][0];
-      assert.strictEqual(args.url, 'testapi');
-      assert.strictEqual(
-        args.formData.mail
-        , JSON.stringify({from: 'from@ehlo.io'})
-      );
+    middleware(
+      {
+        api: 'testapi'
+        , json: {from: 'from@ehlo.io'}
+      }
+      , {}
+      , function apiCallback() {
+        assert.strictEqual(request.post.callCount, 1);
+        var args = request.post.args[0][0];
+        assert.strictEqual(args.url, 'testapi');
+        assert.strictEqual(
+          args.formData.mail
+          , JSON.stringify({from: 'from@ehlo.io'})
+        );
 
-      request
-        .post
-        .restore()
-      ;
-      done();
-    });
+        request
+          .post
+          .restore()
+        ;
+        done();
+      }
+    );
   });
 
   it('parse', function(done) {
-    var middleware = require('../lib/middleware.parse');
-    middleware({}, {}, function() {
+    var middleware = require('../lib/middleware.parse')
+      , mail = {};
+    mail.raw = require('fs').readFileSync('./test/fixtures/mail1.eml');
+    middleware(mail, {}, function parseCallback() {
+      assert.strictEqual(mail.json.text, 'Hello !\n');
+      assert.strictEqual(mail.json.headers.from, 'from@ehlo.io');
+      assert.strictEqual(mail.json.headers.to, 'to@ehlo.io');
+      assert.strictEqual(mail.json.headers['message-id'], '<UUID@EHLO.IO>');
+      assert.strictEqual(mail.json.messageId, 'UUID@EHLO.IO');
+      assert.strictEqual(mail.json.headers.subject, 'SAMPLE');
+      assert.strictEqual(mail.json.subject, 'SAMPLE');
+      assert.strictEqual(mail.json.priority, 'normal');
+      assert.strictEqual(mail.json.from[0].address, 'from@ehlo.io');
+      assert.strictEqual(mail.json.from[0].name, '');
+      assert.strictEqual(mail.json.to[0].address, 'to@ehlo.io');
+      assert.strictEqual(mail.json.to[0].name, '');
       done();
-    }
+    });
   });
 });
